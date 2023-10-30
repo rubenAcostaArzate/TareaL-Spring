b500ebf7d31563b11a7de4ddfca73a205c282fc6
rubenAcostaArzate <rubenacostaarzate@ciencias.unam.mx>
2023-10-30 00:40:27 -0600

    initializr del proyecto

---
+++ demo/src/main/java/com/example/ensaladas/EnsaladasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ensaladas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsaladasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsaladasApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/ensaladas/EnsaladasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ensaladas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsaladasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
